ff76068dde79bd7c9a0d6f99768e8251e0652f2e
Yasmine Kirana <aisyahyasmine96@gmail.com>
2026-05-13 21:54:49 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,21 @@
 CREATE DATABASE transportasi_db;
 USE transportasi_db;
-
 CREATE TABLE users (
  id INT AUTO_INCREMENT PRIMARY KEY,
- username VARCHAR(50),
- password VARCHAR(50)
+ username VARCHAR(50) NOT NULL,
+ password VARCHAR(50) NOT NULL
 );
-
-INSERT INTO users(username,password)
-VALUES('admin','admin123');
+-- Akun untuk tes login
+INSERT INTO users (username, password) VALUES ('admin', 'admin123');
+CREATE TABLE kendaraan (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ plat_nomor VARCHAR(20) NOT NULL,
+ jenis VARCHAR(50) NOT NULL,
+ merk VARCHAR(50) NOT NULL
+);
+CREATE TABLE sopir (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nama VARCHAR(100) NOT NULL,
+ no_sim VARCHAR(30) NOT NULL,
+ no_hp VARCHAR(20) NOT NULL
+);
